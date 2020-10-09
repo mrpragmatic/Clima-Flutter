b0a46e7f9f397aaad2a7f74976f98c483c8ccf9c
mrpragmatic <subhendra.rpandey@gmail.com>
2020-10-09 19:44:04 +0530

    added api http dependency and called api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
 import '../services/location.dart';
-import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,6 +14,13 @@ void getLocation() async {
   print(location.longitude);
 }
 
+void getData() async {
+  var response = await http.get(
+      'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6097d289e10d714a6e88b30761fae22');
+  print(response.statusCode);
+  print(response.body);
+}
+
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
@@ -25,6 +30,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
